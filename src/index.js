e306bac391622a459235bba888ac6626cd2a6940
YasiaDi <112097713+YasiaDi@users.noreply.github.com>
2022-09-14 14:23:49 -0400

    Update index.js
    Inject API results inside HTML

--- src/index.js
+++ src/index.js
@@ -36,31 +36,56 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 
-function displayForecast() {
+let showDate = document.querySelector("#date");
+showDate.innerHTML = `${date} ${month} ${year}`;
+let showTime = document.querySelector("#time");
+showTime.innerHTML = `${day}, ${hours}:${minutes}🕑`;
+
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = [
+  "Sun",
+  "Mon",
+  "Tues",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+];
+return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
  let forecastHTML = `<div class="row">`; 
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6){
      forecastHTML =
       forecastHTML +
             `<div class="col-2">
-              <div class="weather-forecast-weekday">${day}</div>
-              <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="42">
+              <div class="weather-forecast-weekday">${formatDay(forecastDay.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
               <div class="weather-forecast-temp">
-                <span class="weather-forecast-temp-max">23°</span>
-                <span class="weather-forecast-temp-min">16°</span>
+                <span class="weather-forecast-temp-max"> ${Math.round(forecastDay.temp.max)}°</span>
+                <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
               </div>
             </div>`;
+            }
              });
         
  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
-let showDate = document.querySelector("#date");
-showDate.innerHTML = `${date} ${month} ${year}`;
-let showTime = document.querySelector("#time");
-showTime.innerHTML = `${day}, ${hours}:${minutes}🕑`;
+function getForecast(coordinates){
+console.log(coordinates);
+let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -81,14 +106,16 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord) 
+ 
+  
 }
 
 let farengheitTemp = document.querySelector("#farengheit");
 farengheitTemp.addEventListener("click", function (event) {
   event.preventDefault();
   let degreesF = document.querySelector("#degrees");
-    celsiumTemp.classList.remove("active");
-  farengheitTemp.classList.add("active");
   degreesF.innerHTML = Math.round((celsiumTemperature * 9) / 5 + 32);
 });
 
@@ -96,8 +123,6 @@ let celsiumTemp = document.querySelector("#celsium");
 celsiumTemp.addEventListener("click", function (event) {
   event.preventDefault();
   let degreesC = document.querySelector("#degrees");
-  celsiumTemp.classList.add("active");
-  farengheitTemp.classList.remove("active");
   degreesC.innerHTML = Math.round(celsiumTemperature);
 });
 
@@ -135,4 +160,4 @@ let currentLocationButton = document.querySelector("#location-btn");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Kharkiv");
-displayForecast();
+
